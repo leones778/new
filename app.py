f66537ca7c5f2ac75dee31f2268fd0c274e1a67e
leones <228levvol822@gmail.com>
2024-06-17 20:53:05 +0500

    quite well

--- app.py
+++ app.py
@@ -2,89 +2,107 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLAlchemy_DATABASE_URl'] = 'sqlite:///reports.db'
-
-
-db = SQLAlchemy(app)
-
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cartridges.db'
+db.init_app(app)
 
 # Создаем таблицу в базе данных
-class Reportings(db.Model):
+class Сartridges(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    servis = db.Column(db.String(100))
-    report = db.Column(db.String(5000))
+    model = db.Column(db.String(50))
+    printer = db.Column(db.String(100))
+    status = db.Column(db.String(5000))
 
-    def __init__(self, name, servis, report):
-        self.name = name
-        self.servis = servis
-        self.report = report
+    def __init__(self, model, printer, status):
+        self.model = model
+        self.printer = printer
+        self.status = status
 
 
 with app.app_context():
     db.create_all()
 
 
-# Метод для добавления репорта
-@app.route('/add_report', methods=['POST'])
-def add_report():
-    name = request.form['name']
-    servis = request.form['servis']
-    report = request.form['report']
-    rep = Reportings(name, servis, report)
+# Метод для добавления картриджа
+@app.route('/add_cartridges', methods=['POST'])
+def add_cartridges():
+    model = request.form['model']
+    printer = request.form['printer']
+    status = request.form['status']
+    rep = Сartridges(model, printer, status)
     db.session.add(rep)
     db.session.commit()
-    return jsonify({'message': 'Report added successfully'})
+    return jsonify({'message': 'cartridg added successfully'})
 
 
-# Метод для получения отчетов по № id
-@app.route('/viewing_report_fitler_id/<int:id>')
-def viewing_report_fitler_id(id):
-    report = Reportings.query.get(id)
+# Метод для получения картриджей по id
+@app.route('/viewing_cartridges_fitler_id/<int:id>')
+def viewing_cartridges_filter_id(id):
+    report = Сartridges.query.get(id)
     if report:
         return jsonify({
             'id': report.id,
-            'name': report.name,
-            'servis': report.servis,
-            'report': report.report
+            'model': report.model,
+            'printer': report.printer,
+            'status': report.status
         })
     else:
-        return {'error': 'There is no such ID in the list of reports'}
+        return {'error': 'There is no such ID in the list of cartridg'}
 
 
-# Метод для получения отчетов по имени тестера
-@app.route('/viewing_report_fitler_name/<names>')
-def viewing_report_fitler_name(names):
-    reports = Reportings.query.all()
+# Метод для получения кртриджей определенной модели
+@app.route('/viewing_cartridges_filter_model/<model>')
+def viewing_cartridges_filter_model(model):
+    reports = Сartridges.query.all()
     response = []
     for report in reports:
-        if report.name==names:
+        if report.model==model:
             response.append({
                 'id': report.id,
-                'name': report.name,
-                'servis': report.servis,
-                'report': report.report
+                'model': report.model,
+                'printer': report.printer,
+                'status': report.status
             })
     if response:
         return jsonify(response)
     else:
-        return {'error': 'There is no such ID in the list of reports'}
+        return {'error': 'There is no such ID in the list of cartridg'}
 
 
-# Метод для получения всех репортов
-@app.route('/viewing_report', methods=['GET'])
-def viewing_report():
-    reports = Reportings.query.all()
+# Метод для получения кртриджей по статусу
+
+@app.route('/viewing_cartridges_filter_status/<status>')
+def viewing_cartridges_filter_status(status):
+    reports = Сartridges.query.all()
+    response = []
+    for report in reports:
+        if report.status==status:
+            response.append({
+                'id': report.id,
+                'model': report.model,
+                'printer': report.printer,
+                'status': report.status
+            })
+    if response:
+        return jsonify(response)
+    else:
+        return {'error': 'There is no such ID in the list of cartridg'}
+
+
+# Метод для получения всех картриджей
+@app.route('/viewing_cartridges', methods=['GET'])
+def viewing_cartridges():
+    reports = Сartridges.query.all()
     response = []
     for report in reports:
         response.append({
             'id': report.id,
-            'name': report.name,
-            'servis': report.servis,
-            'report': report.report
+            'model': report.model,
+            'printer': report.printer,
+            'status': report.status
         })
     return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
